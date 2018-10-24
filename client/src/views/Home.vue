4465e50e6ea39997951f07ab7685da1c305f59a6
oweissbarth <mail@oweissbarth.de>
2018-10-24 10:36:04 +0200

    added copyright notice

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,7 +4,7 @@
     <p>This is a survey to learn about people's ability to distinguish between photo-realistic computer graphics and photos.</p>
     <p>After answering 2 questions about your background the main part of the survey will start. You will see image and two buttons. Please decide wheter you think the image is a photo or computer graphics. Based on your decision click on the corresponding button.</p>
     <strong>IMPORTANT: IF YOU SAW THE IMAGE BEFORE SEEING IT IN THE SURVEY PLEASE SELECT "I have already seen this" IN THE UPPER RIGHT HAND CORNER.</strong>
-    <p>Privacy: Addtionally to your answers timing informataion is collected. No other information is collected. By starting the survey you agree that all collected data can be stored, processed and published.</p>
+    <p>Notice: Addtionally to your answers timing informataion is collected. No other information is collected. By starting the survey you agree that all collected data can be stored, processed and published. The study is conducted by Oliver Weissbarth. All images are used with permission. Copyright remains with the respective creators.</p>
 
     <br/>
     <router-link to="survey"><button class="startButton" type="button">Start the survey</button></router-link>
